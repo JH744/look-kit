860fe48871d7c84bf200d5113f19422c3c3f4c92
JiHun Kim <dydekffrl@naver.com>
2024-10-12 13:19:07 +0900

    feat : 모든 하위 패키지의 Mapper를 스캔하도록 설정

--- src/main/java/com/example/lookkit/LookkitApplication.java
+++ src/main/java/com/example/lookkit/LookkitApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication//(exclude = DataSourceAutoConfiguration.class)
-@MapperScan(value={"mybatis.dao"})
+@MapperScan("com.example.lookkit")
 public class 	LookkitApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LookkitApplication.class, args);
