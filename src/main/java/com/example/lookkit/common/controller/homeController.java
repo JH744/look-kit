5f705fa1d00281ff1d6ec2790816161b704e528f
JiHun Kim <dydekffrl@naver.com>
2024-10-14 15:56:31 +0900

    feat :인증객체 CustomUser 생성

--- src/main/java/com/example/lookkit/common/controller/homeController.java
+++ src/main/java/com/example/lookkit/common/controller/homeController.java
@@ -1,5 +1,6 @@
 package com.example.lookkit.common.controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -7,7 +8,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class homeController {
 
     @GetMapping("/main")
-    public String mainPage(){
+    public String mainPage(Authentication auth){
+        if(auth != null){
+            System.out.println(auth);
+            System.out.println(auth.getName());
+            System.out.println(auth.getCredentials());
+            System.out.println(auth.getAuthorities());
+            System.out.println(auth.getDetails());
+            System.out.println(auth.isAuthenticated());
+        }
         return "/home/home.html";
     }
 
