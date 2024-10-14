625140f7d2117d794384deaee9a26de949086459
JiHun Kim <dydekffrl@naver.com>
2024-10-14 17:13:02 +0900

    feet : session id저장

--- src/main/java/com/example/lookkit/common/controller/homeController.java
+++ src/main/java/com/example/lookkit/common/controller/homeController.java
@@ -1,5 +1,7 @@
 package com.example.lookkit.common.controller;
 
+import com.example.lookkit.user.CustomUser;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,14 +10,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class homeController {
 
     @GetMapping("/main")
-    public String mainPage(Authentication auth){
+    public String mainPage(Authentication auth, HttpSession session){
         if(auth != null){
             System.out.println(auth);
-            System.out.println(auth.getName());
-            System.out.println(auth.getCredentials());
-            System.out.println(auth.getAuthorities());
-            System.out.println(auth.getDetails());
-            System.out.println(auth.isAuthenticated());
+            CustomUser user = (CustomUser) auth.getPrincipal();
+            session.setAttribute("userid",user.getUserId());
+            // Session의 유효 시간 설정 (3600초 = 60분)
+            session.setMaxInactiveInterval(3600);
+//            System.out.println(user.getUserId());
+//            System.out.println(auth.getName());
+//            System.out.println(auth.getAuthorities());
+//            System.out.println(auth.isAuthenticated());
         }
         return "/home/home.html";
     }
