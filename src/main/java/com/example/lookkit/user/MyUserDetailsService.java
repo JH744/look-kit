a80978023855475a80d1723fd69690f65a03048c
JiHun Kim <dydekffrl@naver.com>
2024-10-14 17:13:02 +0900

    feet : session id저장

--- src/main/java/com/example/lookkit/user/MyUserDetailsService.java
+++ src/main/java/com/example/lookkit/user/MyUserDetailsService.java
@@ -7,7 +7,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,17 +17,19 @@ public class MyUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         UserVO user = UserMapper.getUserByUuid(username);
-        if(user != null ) {
-            System.out.println(user.getUserUuid()+user.getAddress()+user.getGender());
-        }
-        if (user == null) {
-            throw new UsernameNotFoundException("해당 아이디를 가진 사용자가 없습니다: " + username);
-        }
-        List<GrantedAuthority> 권한목록 = new ArrayList<>();
-        권한목록.add(new SimpleGrantedAuthority("ROLE_USER"));
-     // 유저아이디, 유저비밀번호, 유저권한, 유저PK-ID 를 세션에 저장
-     return  new CustomUser(user.getUserUuid(), user.getPassword(), 권한목록,user.getUserId());
+//        if(user != null ) {
+//            System.out.println(user.getUserUuid()+user.getAddress()+user.getGender());
+//        }
+        if (user == null) throw new UsernameNotFoundException("해당 아이디를 가진 사용자가 없습니다: " + username);
 
+        List<GrantedAuthority> userAuthorities = new ArrayList<>();
+        if(user.getRole().equals("ADMIN")){
+            userAuthorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+        }else {
+            userAuthorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+        }
+     // 유저아이디, 유저비밀번호, 유저권한, 유저PK-ID 를 세션에 저장
+     return  new CustomUser(user.getUserUuid(), user.getPassword(), userAuthorities,user.getUserId());
     }
 }
 
