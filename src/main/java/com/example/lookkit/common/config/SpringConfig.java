c13a3cfdd8d343d4885ed99e38431243d3b606da
JiHun Kim <dydekffrl@naver.com>
2024-10-16 01:19:12 +0900

    fix : PasswordEncoder 반환객체 변경

--- src/main/java/com/example/lookkit/common/config/SpringConfig.java
+++ src/main/java/com/example/lookkit/common/config/SpringConfig.java
@@ -2,14 +2,14 @@ package com.example.lookkit.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SpringConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        return new BCryptPasswordEncoder();
     }
 
 }
