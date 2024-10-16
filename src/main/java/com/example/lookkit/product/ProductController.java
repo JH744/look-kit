c8a50c5cd7bc19bc19f490ac97f2b9a92bda9b1a
lxxeugene <lxxeugene@gmail.com>
2024-10-16 12:15:58 +0900

    product detail 생성

--- src/main/java/com/example/lookkit/product/ProductController.java
+++ src/main/java/com/example/lookkit/product/ProductController.java
@@ -1,7 +1,22 @@
 package com.example.lookkit.product;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ProductController {
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/product/{id}")
+    public String productPage(@PathVariable("id") int id, Model model) {
+        ProductVO product = productService.getProductById(id);
+        model.addAttribute("product", product);
+        return "product";
+    }
 }
