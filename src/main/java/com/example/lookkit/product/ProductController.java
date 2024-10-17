90a78652eb21696b7bc66cdbdc30fbf3bc5a17fd
lxxeugene <lxxeugene@gmail.com>
2024-10-17 11:14:43 +0900

    리뷰 생성

--- src/main/java/com/example/lookkit/product/ProductController.java
+++ src/main/java/com/example/lookkit/product/ProductController.java
@@ -1,22 +1,36 @@
 package com.example.lookkit.product;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
+import com.example.lookkit.review.ReviewService;
+import com.example.lookkit.review.ReviewVO;
 
 @Controller
 public class ProductController {
-    private final ProductService productService;
 
-    public ProductController(ProductService productService) {
+
+    private final ProductService productService;
+    private final ReviewService reviewService;
+
+    @Autowired
+    public ProductController(ProductService productService, ReviewService reviewService) {
         this.productService = productService;
+        this.reviewService = reviewService;
     }
+
 
     @GetMapping("/product/{id}")
     public String productPage(@PathVariable("id") int id, Model model) {
         ProductVO product = productService.getProductById(id);
+        List<ReviewVO> reviews = reviewService.getReviewsByProductId(id);
+
         model.addAttribute("product", product);
-        return "product";
+        model.addAttribute("reviews", reviews);
+        return "product"; 
     }
+    
 }
